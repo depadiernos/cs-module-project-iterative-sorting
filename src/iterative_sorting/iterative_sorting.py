de2692857aae6c464240cc5cdcb1d41fe6840376
Dennis Padiernos <depadiernos@gmail.com>
2020-07-08 22:10:21 -0500

    fix selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,12 +1,11 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(len(arr)):
         cur_index = i
         smallest_index = cur_index
 
-        for j in range(i+1, len(arr) - 1):
-            if arr[j] < arr[smallest_index]:
+        for j in range(i, len(arr)):
+            if arr[j] <= arr[smallest_index]:
                 smallest_index = j
 
         arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
